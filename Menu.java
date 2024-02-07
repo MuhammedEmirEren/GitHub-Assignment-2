8aaccdf54971217945bd0d926ed5bff2fcbee92e
Furkan Karademir <furkan.karademir@ug.bilkent.edu.tr>
2024-02-07 11:07:49 +0300

    Step 2
    menu is done

--- Menu.java
+++ Menu.java
@@ -17,10 +17,36 @@ public class Menu {
 
         ListOperation list = new ListOperation(size);
 
-        System.out.println(Arrays.toString(list.getList()));
 
-        System.out.println(list.getMax());
-        System.out.println(list.getMin());
-        System.out.println(Arrays.toString(list.averageFinding()));
+        int choice = 0;
+
+        while (choice != -1)
+        {
+            System.out.println();
+            System.out.println("Enter 1 to find the minimum and the maximum of the array");
+            System.out.println("Enter 2 to find the average of the array");
+            System.out.println("Enter 3 to find the sum of elements with odd- and even-numbered indexes");
+            System.out.println("Enter -1 to exit");
+
+            System.out.print("Choice: ");
+
+            choice = input.nextInt();
+            System.out.println();
+
+            if (choice == 1)
+            {
+                System.out.println("The minimum of the array is: " + list.getMin());
+                System.out.println("The maximum of the array is: " + list.getMax());
+            }
+            else if (choice == 2)
+            {
+                System.out.println("Average of the array i: " + list.averageFinding());
+            }
+            else if (choice == 3)
+            {
+                System.out.println("The sum of elements with even-numbered indexes: " + list.sumOfEvenIndexes());
+                System.out.println("The sum of elements with odd-numbered indexes: " + list.sumOfOddIndexes());
+            }
+        }
     }
 }
